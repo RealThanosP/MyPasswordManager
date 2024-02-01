243bcde79cb80c8cff1e154eb9750f0689ef8819
RealThanosP <panathanos1@gmail.com>
2024-02-01 20:26:41 +0200

    Minor changes

--- encryption.py
+++ encryption.py
@@ -5,22 +5,9 @@ import os
 # NOTE: The encrtyption of the files is being done in the db folder
 # All the keys will be passed in the db folder and be loaded from there
 
-def generate_key(vault_name:str):
-    '''Generates a key into the app folderto use to encrypt/decrypt'''
-    APP_FOLDER = db_folder()
+def generate_key():
+    '''Generates a key into the app folder to use to encrypt/decrypt'''
     key = Fernet.generate_key()
-    key_name = f"{vault_name}.key"
-    key_dir = os.path.join(APP_FOLDER, key_name)
-
-    # # Check for the key (CHANGE IT TO LINK TO A DATABASE TABLE)
-    # if os.path.exists(key_dir):
-    #     with open(key_dir, "rb") as vault_key:
-    #         key = vault_key.read()
-    # else:
-    #     key = Fernet.generate_key()
-    #     with open(key_dir, "wb") as vault_key:
-    #         vault_key.write(key)
-    
     return key
 
 def encrypt_data(key:bytes, data:str):
@@ -30,7 +17,7 @@ def encrypt_data(key:bytes, data:str):
     
     return encrypted_data
 
-def encrypt_list(key, input_list):
+def encrypt_list(key, input_list:list):
     for index, info in enumerate(input_list):
         input_list[index] = encrypt_data(key, info)
     
@@ -41,7 +28,7 @@ def decrypt_data(key:bytes, data:bytes):
     decrypted_data = Fernet(key).decrypt(data)
     return decrypted_data.decode('utf-8')
 
-def decrypt_nested_list(accounts:list):
+def decrypt_nested_list(accounts:list):# acounts is :[(username, password, service, notes, key), (...)]
     for i, account_info in enumerate(accounts):
             accounts[i] = list(account_info)
             for index, element in enumerate(account_info[:-1]):
@@ -62,29 +49,3 @@ def db_folder():
     
 
     return app_folder
-
-if __name__ == '__main__':
-    # Example code for actual use
-    APP_FOLDER = db_folder()
-    VAULT_NAME = "Test"
-    
-    key_bytes = generate_key(VAULT_NAME)
-    key_str = str(key_bytes)
-    print(type(key_str))
-    data = "I am a hacker"
-
-    encrypted = encrypt_data(key_bytes, data)
-    print(encrypted)
-    decrypted = decrypt_data(key_bytes, encrypted)
-    print(decrypted)
-
-    
-    
-
-
-
-
-    
-    
-
-            
