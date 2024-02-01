74becb562e2f72c83d3e112f9381fa7d79e943be
RealThanosP <panathanos1@gmail.com>
2024-02-01 20:28:19 +0200

    Stores data succesfully and space names fixed

--- database.py
+++ database.py
@@ -2,6 +2,13 @@ import encryption
 import sqlite3
 import os
 
+# NOTE: The name of the vault should not have spaces
+def db_name(name:str):
+    '''Returns the name version that can be a table name in the db'''
+    name_elements = name.split(" ")
+    shaped_name = "_".join(name_elements)
+    return shaped_name
+
 def db_path():
     '''Returns the db path for the connection'''
     DB_NAME = "Account_services.db"
@@ -10,45 +17,95 @@ def db_path():
     DB_PATH = os.path.join(DB_FOLDER, DB_NAME)
     return DB_PATH
 
-def create_vault(vault_name):
+def get_all_vaults():
+    '''Returns a nested list with all the vaults in the database'''
+    with sqlite3.connect(db_path()) as conn:
+        cur = conn.cursor()
+
+        cur.execute(f"SELECT name FROM sqlite_master WHERE type='table'")
+
+        vaults = cur.fetchall()
+        
+        for index, vault in enumerate(vaults):
+            vaults[index] = vault[0].replace("_", " ")
+
+    return vaults
+
+def create_vault(vault_name:str):
     '''Initialize the vault table'''
     with sqlite3.connect(db_path()) as conn:
         cur = conn.cursor()
         
-        cur.execute(f'''CREATE TABLE IF NOT EXISTS {vault_name}(
+        cur.execute(f'''CREATE TABLE IF NOT EXISTS {db_name(vault_name)}(
                     username TEXT,
                     password TEXT,
                     service TEXT,
+                    notes TEXT,
                     key TEXT
         )''')
         conn.commit()
 
-def store_account(key:bytes, vault_name:str, account:list):
-    '''Stores an account details with the key '''
-    encrypted_list = encryption.encrypt_list(key, account)
-    create_vault(vault_name)
+def check_for_dublicate_account(vault_name:str, username:str, service:str):
+    '''Returns True when there is another account in the db and False when there is not another acc'''
     with sqlite3.connect(db_path()) as conn:
         cur = conn.cursor()
+        
+        cur.execute(f'SELECT * FROM {db_name(vault_name)}')
 
-        cur.execute(f'''INSERT INTO {vault_name} VALUES (?,?,?,?)''', tuple(encrypted_list))
+        acc = cur.fetchall()
+        accounts = encryption.decrypt_nested_list(acc)
+        name_service_db = []
 
-        conn.commit()   
+        for acc in accounts:
+            acc.pop(1)
+            acc.pop(-1)
+            name_service_db.append(acc)
+        
+        if [username, service] in name_service_db:
+            return True
+        
+        return False
+
+def store_account(key:bytes, vault_name:str, decrypted_account:list):
+    '''Stores the encrypted account details with the key. Returns None when the account is not saved and True when it is.'''
+    is_account_in_db = check_for_dublicate_account(vault_name, decrypted_account[0], decrypted_account[2])
+    encrypted_list = encryption.encrypt_list(key, decrypted_account)
+
+    with sqlite3.connect(db_path()) as conn:
+        cur = conn.cursor()
+        # Checks for dublicate accounts in the db
+        if is_account_in_db: 
+            return None
+
+        cur.execute(f'''INSERT INTO {db_name(vault_name)} VALUES (?,?,?,?,?)''', tuple(encrypted_list))
+        conn.commit()
+
+        return True
 
 def get_vault(vault_name:str):
     '''Returns a nested list with all the vaults accounts'''
     with sqlite3.connect(db_path()) as conn:
         cur = conn.cursor()
         
-        cur.execute(f'''SELECT * FROM {vault_name}''')
+        cur.execute(f'''SELECT * FROM {db_name(vault_name)}''')
 
-        
         accounts = cur.fetchall()
         acc = encryption.decrypt_nested_list(accounts)
              
         conn.commit()
 
     return acc
-                             
+
+def get_username_and_service_values(vault_name:str):
+    vault = get_vault(vault_name)
+    wanted = []
+    for row in vault:
+        row.pop(1)
+        row.pop(2)
+        wanted.append(row)
+    
+    return wanted
+
 def drop_table(table_name):
     '''Delete the table you insert'''
     with sqlite3.connect(db_path()) as conn:
@@ -76,8 +133,17 @@ def drop_all_tables():
         # Commit the changes
         conn.commit()
 
+# Just for testing
 if __name__ == '__main__':
-    key = encryption.generate_key("Vault")
+    vault_name = "Start Vault"
+    account = ["123", "123asdasd", "1232", "123asdasd"]
+
+    key = encryption.generate_key()
+
+    create_vault(vault_name)
+    store_account(key, vault_name, account)
+    print(get_vault(vault_name))
+
 
 
 
